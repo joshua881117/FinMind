e57af29fe7fc9574d23215452db7c31832eab080
林書亞 <linshuya@linshuyadeMacBook-Air.local>
2023-09-11 10:04:45 +0800

    change network name

--- DataEngineering/Chapter7/7.2/7.2.2/api/main.py
+++ DataEngineering/Chapter7/7.2/7.2.2/api/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from fastapi import FastAPI
-from sqlalchemy import create_engine, engine
+from sqlalchemy import create_engine, engine, text
 from api import config
 
 
@@ -34,7 +34,9 @@ def taiwan_stock_price(
     and Date>= '{start_date}'
     and Date<= '{end_date}'
     """
-    mysql_conn = get_mysql_financialdata_conn()
-    data_df = pd.read_sql(sql, con=mysql_conn)
-    data_dict = data_df.to_dict("records")
+    with get_mysql_financialdata_conn() as conn:
+        sql_query = text(sql)
+        data_df = pd.read_sql(sql_query, con=conn)
+        conn.commit()
+        data_dict = data_df.to_dict("records")
     return {"data": data_dict}
